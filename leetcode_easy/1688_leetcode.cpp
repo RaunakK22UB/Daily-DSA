bd8c770d9747fb79ac6c04dc9cc94921b66244c2
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-08-05 23:13:55 +0530

    5-08-25

---
+++ leetcode_easy/1688_leetcode.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+             int matches=0;
+             int team=n;
+             int ans=0;
+             while(team > 1){
+                if(team % 2!=0){
+                    matches = (team-1)/2;
+                    team= ((team-1)/2)+1;
+                }else{
+                   matches = team/2;
+                    team= team/2;
+                }
+                ans +=matches;
+             }
+
+             return ans;
+    }
+};
