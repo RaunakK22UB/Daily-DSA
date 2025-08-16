9f317df4f19babca7d022d64dbd3d3aba592456a
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-08-16 20:33:29 +0530

    16-8-25

--- greedy/2405_leetcode.cpp
+++ greedy/2405_leetcode.cpp
@@ -13,4 +13,11 @@ public:
                 if(last[ch - 'a'] >= start){
                        count++;
                        start = i;
-     
+                      
+                }
+                 last[ch - 'a']=i;  // for each char we have to update the vector that's why outside the if cond^n
+          }
+
+          return count + 1;  // why count + 1 try to dry run the example 1   "abacaba" ab ac ab a  when i will be at last a index 6 we will check for this condition last[ch - 'a'] >= start but count will be for this ab substring  , i will be move ahead and out of bound thats why for that substaring we are adding 1
+    }
+};
