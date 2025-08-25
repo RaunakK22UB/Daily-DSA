29eebb69b9a7ef3971fe0e66f8c0f33507be0f54
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-08-25 22:36:19 +0530

    25-8-25

---
+++ binary Search/leetcode_374.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+           int start = 1;
+           int end = n;
+           while(start <= end){
+                int guess_number = start + (end-start)/2;
+
+                int val = guess(guess_number);
+
+                if(val == -1){
+                    end = guess_number - 1; // mid -1
+                }
+                else if(val == 1){
+                     start = guess_number + 1; // mid + 1
+                }else{
+                      return guess_number;
+     
