8852f8a12f22eff55ea1cdf7fbc53597f4083ae4
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-09-02 23:33:11 +0530

    2-09-2025

--- Queue/gfg_reversal_of_queue.cpp
+++ Queue/gfg_reversal_of_queue.cpp
@@ -29,3 +29,45 @@ class Solution {
           q.pop();
           st.push(element);
         
+// function Template for C++
+
+class Solution {
+  public:
+    queue<int> reverseQueue(queue<int> &q) {
+        
+        
+        
+        // --------------------by the help of recursion 
+//         //base condition 
+        
+//         if(q.empty()){
+//             return q;
+//         }
+        
+//         int element = q.front();
+//         q.pop();
+//         reverseQueue(q);
+//         q.push(element);
+//             return q;
+//     }
+// };
+
+//-----------by the help of stack
+        
+        stack<int>st;
+        while(!q.empty()){
+            int element = q.front();
+          q.pop();
+          st.push(element);
+        
+        }
+        while(!st.empty()){
+            int element = st.top();
+          st.pop();
+          q.push(element);
+        
+        }
+        
+        return q;
+    }
+};
