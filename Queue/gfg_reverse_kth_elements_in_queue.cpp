9780d4ecdaddb73bb6922925cd49d0077ab0d017
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-09-03 23:09:31 +0530

    3-09-2025

---
+++ Queue/gfg_reverse_kth_elements_in_queue.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    queue<int>reverseFirstK(queue<int>q, int k) {
+        // code here
+        stack<int>st;
+        for(int i=0;i<k;i++){
+               int element = q.front();
+               q.pop();
+               st.push(element);
+        }
+        
+        while(!st.empty()){
+            int element = st.top();
+            st.pop();
+            q.push(element);
+        }
+        
+     
