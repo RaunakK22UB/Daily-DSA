c0c884a20b6c824fd8b30d28f4eabb0a068df609
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-09-06 23:59:25 +0530

    6-09-2025

---
+++ Dynamic programming/1105_leetcode.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+   int OriginalShelfWidth;
+   int solve(vector<vector<int>>& books, int idx,int remainingWedith,int maxHeight,vector<vector<int>>dp){
+           
+           // Base Cond^n
+           int n =books.size();
+           if(idx >=n){
+              return maxHeight;
+           }
+           if(dp[idx][remainingWedith] !=-1){
+              return dp[idx][remainingWedith];
+           }
+
+
+           int bookW = books[idx][0];
+           int bookH = books[idx][1];
+           int keep =INT_MAX;
+           int skip =INT_MAX;
+
+           //keeep
+           if(bookW <= remainingWedith){
+               keep = solve(books,idx+1,remainingWedith-bookW,max(maxHeight,bookH),dp);
+           }
+
+           //skip
+
+           skip = maxHeight + solve(books,idx+1,OriginalShelfWidth-bookW,bookH,dp);
+
+
+           return dp[idx][remainingWedith] = min(keep,skip);
+
+
