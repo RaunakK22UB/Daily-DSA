7ff90b2bd573b477820e46c8fa97a33f415834a1
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-09-07 23:01:24 +0530

    07-09-2025

---
+++ leetcode_easy/1304_leetcode.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int>ans(n);
+        
+        if(n%2==0){
+            int num=1;
+            for(int i=0;i<n;i+=2){
+                   ans[i]=num;
+                   ans[i+1]= -num;
+                   num++;
+            }
+        }
+        else{
+            int num=1;
+            ans[n-1] = 0;
+            for(int i=0;i<n-1;i+=2){
+                   ans[i]=num;
+                   ans[i+1]= -num;
+  
