1ce1ad45d5c22525c46da08ef3cddabdba8316b7
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-09-18 21:25:51 +0530

    18-9-2025

--- backtracking/1219_leetcode.cpp
+++ backtracking/1219_leetcode.cpp
@@ -34,4 +34,15 @@ public:
     int getMaximumGold(vector<vector<int>>& grid) {
            n = grid.size(); // rows
            m = grid[0].size(); // columns
-     
+        int maxResult =0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                   if(grid[i][j]!=0){
+                      maxResult = max(maxResult , solve(grid, i , j));
+                   }
+            }
+        }
+
+        return maxResult;
+    }
+};
