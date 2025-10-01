8fce7eeb97c75bd370e95f4b8201c7cd679fc340
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-10-01 22:27:30 +0530

    1-10-2025

---
+++ BST/leetcode_102.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;   
+    vector<int>eachLevel;
+    queue<TreeNode*>q;
+    if(root==NULL) return ans;
+	q.push(root);
+	q.push(NULL);
+	while(!q.empty()){
+	      TreeNode* temp = q.front();
+	      q.pop();
+	      
+	      if(temp == NULL){
+		  ans.push_back(eachLevel); // means this level is doen we will move to next level
+          eachLevel.clear(); // for that clear the data insead this vector
+	      	   if(!q.empty()){
+	      	   	    q.push(NULL);
+				}
+		  }
+		  else{
+            eachLevel.push_back(temp->val); // collect the data members in that level
+		  	if(temp->left){
+	      	q.push(temp->left);
+		  }
+		  if(temp->right){
+		  	q.push(temp->right);
+		  }
+		  	
+		  }
+	      
+	}
+    return ans;
+    }
+};
