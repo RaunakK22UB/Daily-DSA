99b20982bb3edfb5fff4ed5d91c92bac22d08b22
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-10-07 22:12:43 +0530

    7-10-2025

--- BST/199_leetcode.cpp
+++ BST/199_leetcode.cpp
@@ -20,3 +20,41 @@ public:
            return ans;
     }
 };
+
+
+//-----------BFS------------using level oder traversal
+
+
+
+// vector<int> rightSideView(TreeNode* root) {
+//            vector<int>ans;
+//         //    solve(root,0,ans);
+//         //    return ans;
+
+//         // using level oder traversal
+
+//         if(root == NULL ) return ans;
+
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty()){
+//             int n = q.size();
+//             TreeNode* temp = NULL;
+//             while(n--){
+//                   temp = q.front();
+//                   q.pop();
+
+//                   if(temp->left !=NULL){
+//                     q.push(temp->left);
+//                   }
+//                   if(temp->right !=NULL){
+//                     q.push(temp->right);
+//                   }
+//             }
+//              ans.push_back(temp->val);
+
+//         }
+
+//         return ans;
+//     }
+// };
