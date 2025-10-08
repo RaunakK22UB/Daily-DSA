2c9bd3db1128cd3cb97a6d1bef5bc5f8ed7c8cd3
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-10-08 18:19:31 +0530

    08-10-2025

--- BST/958_leetcode.cpp
+++ BST/958_leetcode.cpp
@@ -25,3 +25,32 @@ public:
     }
 };
 
+class Solution {
+public:
+//--------------------------------------------------using DFS or preorder
+    int totalNodes(TreeNode* root){
+            if(root == NULL) return 0;
+
+            return 1 + totalNodes(root->left) + totalNodes(root->right);
+    }
+    bool solve(TreeNode * root , int idx , int totalNode ){
+        if(root == NULL) return true;
+
+        if( idx > totalNode) return false; // jaha idx jada hua totalNode se to return bhai false
+
+        return solve(root->left, 2*idx,totalNode) && solve(root->right,2*idx+1,totalNode);
+        // this are traversing the left and right and chceking the base condition 
+    }
+    bool isCompleteTree(TreeNode* root) {
+          
+          if(root == NULL) return true;
+
+          int totalNode = totalNodes(root);
+
+          int idx = 1; // index starts form 1 for the binary tree for left 2 * i and for right [2 * i + 1] index for any node   , by this we can create the array from a binary tree tree do dry run for non complete binary tree you will get index more then number of nodes , because there will be one node will be null
+
+          return solve(root , idx, totalNode);
+
+
+    }
+};
